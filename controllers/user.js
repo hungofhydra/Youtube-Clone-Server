9d705e2147d2663474472fc97099e7b5f5f46c7f
Nguyen Tan Hung <hungofhydraforwork@gmail.com>
2022-08-02 15:48:25 +0700

    Add ErrorHandler, Login API and Register API

---
+++ controllers/user.js
@@ -0,0 +1,7 @@
+const test = (req, res) => {
+    res.json('test');
+}
+
+module.exports = {
+    test
+}
--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 
 const connectDB = require('./db/connectDB');
+const userRoute = require('./routes/users');
+const videosRoute = require('./routes/videos');
+const commentRoute = require('./routes/comments');
+const authRoute = require('./routes/auths');
+const errorHandler = require('./errors/errorHandler');
 
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 const app = express()
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/video', videosRoute);
+app.use('/api/v1/comment', commentRoute);
+app.use(errorHandler);
 
 const start = async () => {
     try {
